b8022147b8f18f06372b5657a3d54ed23f7fe7d6
Vetrivel M <chanranpandiyammal@gmail.com>
2024-08-13 15:56:49 +0530

    Initial commit - Added project for finding sum of even numbers in an array

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of Even Numbers in Array");
+            Console.WriteLine("Enter the Element Number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] number = new int[n];
+            Console.WriteLine("Enter the Elements in an Array");
+            for (int i = 0; i < n; i++)
+            {
+                number[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            foreach (int i in number)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine("Sum of the Even Numbers in Array: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
